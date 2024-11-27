a8bbeb013edb1310d79510ea0ebb198b6b3de664
Yuha Yabuki <zeal22300050@fsgcl.jp>
2024-11-27 16:07:08 +0900

    紅の木の自動生成完成

--- src/main/java/com/servent/redage/worldgen/biome/RedAgeBiomeModifiers.java
+++ src/main/java/com/servent/redage/worldgen/biome/RedAgeBiomeModifiers.java
@@ -12,12 +12,15 @@ import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class RedAgeBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_ZINC_ORE = createKey("add_zinc_ore");
+
+    public static final ResourceKey<BiomeModifier> ADD_RED_TREE = createKey("add_red_tree");
 
     public static void bootstrap(BootstapContext<BiomeModifier> context) {
         HolderGetter<PlacedFeature> placedFeatures = context.lookup(Registries.PLACED_FEATURE);
@@ -27,6 +30,11 @@ public class RedAgeBiomeModifiers {
                 new ForgeBiomeModifiers.AddFeaturesBiomeModifier(biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
                         HolderSet.direct(placedFeatures.getOrThrow(RedAgePlacement.ORE_ZINC)),
                         GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_RED_TREE,
+                new ForgeBiomeModifiers.AddFeaturesBiomeModifier(biomes.getOrThrow(Tags.Biomes.IS_PLAINS),
+                        HolderSet.direct(placedFeatures.getOrThrow(RedAgePlacement.RED_TREE)),
+                        GenerationStep.Decoration.VEGETAL_DECORATION));
     }
 
     private static ResourceKey<BiomeModifier> createKey(String name) {
