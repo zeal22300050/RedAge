4562c5a5c668ed22422c41081432a76f37321cd9
Yuha Yabuki <zeal22300050@fsgcl.jp>
2024-11-27 15:31:28 +0900

    TreeGrowerの追加

---
+++ src/main/java/com/servent/redage/worldgen/tree/RedTreeGrower.java
@@ -0,0 +1,15 @@
+package com.servent.redage.worldgen.tree;
+
+import com.servent.redage.worldgen.features.RedAgeFeatures;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.grower.AbstractTreeGrower;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import org.jetbrains.annotations.Nullable;
+
+public class RedTreeGrower extends AbstractTreeGrower {
+    @Override
+    protected @Nullable ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(RandomSource randomSource, boolean b) {
+        return RedAgeFeatures.RED_TREE_KEY;
+    }
+}
